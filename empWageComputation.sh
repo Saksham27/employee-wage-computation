d5f98dfd5e6d8c3618d007567b1450dfb1f8c0af
Saksham27 <geekysaksham27@gmail.com>
2020-03-17 15:33:25 +0530

    Added: Employee attendance feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "** Welcome to Employee Wage Computation **"
+
+# function to check if employee is present or absent
+function employeeAttendance() {
+	local counter=$(( RANDOM%2 ))
+	if [ $counter -eq 1 ]
+	then
+		echo "present"
+	else
+		echo "absent"
+	fi
+}
+
+employeeAttendance
