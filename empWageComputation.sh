0d4d099b1a4b57352fc6e9d3032fb755b5101108
Saksham27 <geekysaksham27@gmail.com>
2020-03-17 18:54:58 +0530

    Added: Employee daily wage calculation feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -x
 
+#CONSTANTS
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+
+#variables
+isEmployeePresent
+wageForADay
+
 echo "** Welcome to Employee Wage Computation **"
 
 # function to check if employee is present or absent
@@ -13,4 +21,19 @@ function employeeAttendance() {
 	fi
 }
 
-employeeAttendance
+# funcion to calculate employee wage
+# param1 : wage per hour
+# param2 : working hours of employee
+function dailyEmployeeWage() {
+	echo $(( $1*$2 ))
+}
+
+
+isEmployeePresent=$( employeeAttendance )
+
+if [ $isEmployeePresent = "present" ]
+then
+	wageForADay=$( dailyEmployeeWage $WAGE_PER_HOUR $FULL_DAY_HOUR )
+else 
+	wageforADay=0
+fi
