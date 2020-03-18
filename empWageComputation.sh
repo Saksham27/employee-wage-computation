d0b4c951b93b50961807ada829a1c22dc7eef3c0
Saksham27 <geekysaksham27@gmail.com>
2020-03-18 14:51:47 +0530

    Added: wage calculation for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,12 +8,16 @@ FULL_TIME_EMPLOYEE=1
 PART_TIME_EMPLOYEE=0
 EMPLOYEE_PRESENT=1
 EMPLOYEE_ABSENT=0
+MONTHLY_WORKING_DAYS=20
+
 #variables
 isEmployeePresent
 wageForADay
 employeeType
 employeeWorkingTime
 workingHoursForDay
+daysEmployeeWorkedForMonth=0
+employeeMonthlyWage=0
 
 # function to check if employee is present or absent
 # local variables
@@ -58,22 +62,30 @@ function employeeType() {
 
 echo "** Welcome to Employee Wage Computation **"
 
-isEmployeePresent=$( employeeAttendance )
+while [ $daysEmployeeWorkedForMonth -lt $MONTHLY_WORKING_DAYS ]
+do
+	isEmployeePresent=$( employeeAttendance )
 
-if [ $isEmployeePresent = "present" ]  # cheking if employee is present or not
-then
-	employeeWorkingTime=$( employeeType )  # setting employee's work time, part time or full time accordingly
+	if [ $isEmployeePresent = "present" ]  # cheking if employee is present or not
+	then
+		employeeWorkingTime=$( employeeType )  # setting employee's work time, part time or full time accordingly
 
-	case $employeeWorkingTime in  # setting working hours for a day according to employee type
-		fulltime)
-			workingHoursForDay=$FULL_DAY_HOUR
-			;;
-		parttime)
-			workingHoursForDay=$PART_TIME_HOUR
-			;;
-	esac
+		case $employeeWorkingTime in  # setting working hours for a day according to employee type
+			fulltime)
+				workingHoursForDay=$FULL_DAY_HOUR
+				;;
+			parttime)
+				workingHoursForDay=$PART_TIME_HOUR
+				;;
+		esac
 
-	wageForADay=$( dailyEmployeeWage $WAGE_PER_HOUR $workingHoursForDay )  # calculating a day's wage for the employee if present
-else
-	wageForADay=0 # caculating a day;s wage for employee if absent
-fi
+		wageForADay=$( dailyEmployeeWage $WAGE_PER_HOUR $workingHoursForDay )  # calculating a day's wage for the employee if present
+	else
+		wageForADay=0 # caculating a day;s wage for employee if absent
+	fi
+
+	employeeMonthlyWage=$(( $employeeMonthlyWage+$wageForADay))
+	(( daysEmployeeWorkedForMonth++ ))
+done
+
+echo "Employee Wage for the month : $employeeMonthlyWage"
